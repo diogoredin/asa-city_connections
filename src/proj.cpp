26b8d2c58f2e76e40c638e8066a1da936c4f6d1c
Keyaku <antonio.luis.sarmento@gmail.com>
2017-04-24 15:38:02 +0100

    Cleaning up unnecessary code

--- src/proj.cpp
+++ src/proj.cpp
@@ -63,33 +63,9 @@ class Graph {
 		friend std::ostream& operator<<(std::ostream& os, const Graph &graph);
 };
 
-Graph::Graph(int num_v, int num_e) {
-	Vertex u;
-
-	_status   = INCOHERENT;
-	_first    = std::vector<Edge>(num_v+1);
-	_vertex   = std::vector<Vertex>(num_e+1);
-	_next     = std::vector<Edge>(num_e+1);
-
-	_indegree = std::vector<int>(num_v+1);
-
-	for (_nr_edges = 1; _nr_edges <= num_e; _nr_edges++) {
-		int num1, num2;
-		Vertex v;
-
-		get_numbers(&num1, &num2);
-		u = new_vertex(num1);
-		v = new_vertex(num2);
-
-		connect(u, v);
-	}
-
-	/* Adds all orphans to Queue */
-	for (u = 1; u <= num_v; u = next_vertex(u)) {
-		if (_indegree[u] == 0) {
-			enqueue(u);
-		}
-	}
+Graph::Graph(int num_vertices) {
+	// TODO
+	_status = INSUFFICIENT;
 }
 Graph::~Graph() { /* Nothing here */ }
 
@@ -126,33 +102,7 @@ std::ostream& operator<<(std::ostream& os, const Graph &graph) {
 
 /************************* Vertex "Deletion" Algorithm ***************************/
 void Graph::sort() {
-	while ( !is_empty() ) {
-		Vertex u = dequeue();
-		_result.push_back(u);
-
-		if ( _indegree[u] == 0 ) {
-			int max_solutions = 0;
-
-			for ( Edge find_son = _first[u]; find_son != 0; find_son = _next[find_son] ) {
-				Vertex v = _vertex[find_son];
-
-				if ( --_indegree[v] == 0 ) {
-					enqueue(v);
-					max_solutions++;
-				}
-
-				/* If suddenly u has more than 1 adjacent whose _indegree == 0 */
-				if (max_solutions > 1) {
-					_status = INSUFFICIENT;
-					return;
-				}
-			}
-		}
-	}
-
-	if ( _result.size() == _first.size() - 1 ) {
- 		_status = CORRECT;
- 	}
+	// TODO
 }
 
 /***************************** MAIN function **********************************/
