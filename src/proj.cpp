eadfee217acf1a3a07ac7d494f56ffe1257bb85b
Keyaku <antonio.luis.sarmento@gmail.com>
2017-04-29 16:04:21 +0100

    Redefined a few structures
    Done for faster performance and less memory consumption.
    
    Changes:
    - Converted a couple of C++ vectors to arrays
    - Added Budget structure (which cleans some boilerplate
    - Corrected a visited[] state
    - Minor code corrections

--- src/proj.cpp
+++ src/proj.cpp
@@ -38,11 +38,15 @@ Vertex new_vertex(int val) { return val; }
 
 /* Edge Structure */
 typedef pair< int, pair<Vertex, Vertex> > Edge;
-
-/* Build Edge */
 Edge new_edge(Vertex city_a, Vertex city_b, int cost) {
 	return make_pair(cost, make_pair(city_a, city_b));
 }
+
+/* Project Budget Structure */
+typedef struct {
+	int cost;
+	int num_roads, num_airports;
+} Budget;
 
 /* Compare Edges */
 struct GreaterEdge { // Returns true if edge_a comes after edge_b
@@ -61,21 +65,20 @@ class Graph {
 	private:
 		Status _status;
 		size_t _num_vertices;
-		int _total_cost;
-		int _final_roads, _final_airports;
+		Budget _budget;
 
 		priority_queue< Edge, vector<Edge>, GreaterEdge > _edges;
-		vector<int> _rank;
-		vector<Vertex> _parent;
+		int *_rank;
+		Vertex *_parent;
 
 	public:
 		Graph(int num_vertices);
 		~Graph();
 
 		/* Class attribute methods */
-		int cost()         const { return _total_cost; }
-		int num_roads()    const { return _final_roads; }
-		int num_airports() const { return _final_airports; }
+		int cost()         const { return _budget.cost; }
+		int num_roads()    const { return _budget.num_roads; }
+		int num_airports() const { return _budget.num_airports; }
 		Status status()    const { return _status; }
 		size_t size()      const { return _num_vertices; }
 
@@ -112,17 +115,17 @@ class Graph {
 /* Builds Graph */
 Graph::Graph(int num_vertices) {
 
-	_rank.resize(num_vertices + 1);
-	_parent.resize(num_vertices + 1);
+	_rank = new int[num_vertices + 1];
+	_parent = new Vertex[num_vertices + 1];
 
 	_status = CORRECT;
 	_num_vertices = num_vertices;
-	_total_cost = 0;
-	_final_airports = 0;
-	_final_roads = 0;
 
 }
-Graph::~Graph() { /* Nothing here */ }
+Graph::~Graph() {
+	delete [] _rank;
+	delete [] _parent;
+}
 
 /* Examines Graph */
 ostream& operator<<(ostream& os, const Graph &graph) {
@@ -145,10 +148,7 @@ void Graph::min_span_tree(void) {
 	/* Shared variables */
 	int cost;
 	Vertex city_a, city_b;
-	struct {
-		int cost;
-		int num_roads, num_airports;
-	} roads = { 0, 0, 0 }, roads_airports = { 0, 0, 0 };
+	Budget roads = { 0, 0, 0 }, roads_airports = { 0, 0, 0 };
 
 	/* MST has no airports */
 	for ( Vertex city = 1; city <= _num_vertices; city++ ) {
@@ -156,7 +156,7 @@ void Graph::min_span_tree(void) {
 	}
 
 	for ( ; !_edges.empty(); _edges.pop() ) {
-		edges2.push(_edges.top()); /* FIXME: Stop using this hack and clone the queue */
+		edges2.push(_edges.top());
 
 		cost = _edges.top().first;
 		city_a = _edges.top().second.first;
@@ -178,7 +178,7 @@ void Graph::min_span_tree(void) {
 	}
 
 	/* MST has airports */
-	vector<bool> visited(_num_vertices + 1);
+	bool *visited = new bool[_num_vertices + 1];
 	for ( Vertex city = 0; city <= _num_vertices; city++ ) {
 		make_set(city);
 	}
@@ -195,14 +195,13 @@ void Graph::min_span_tree(void) {
 
 			merge_set(set_a, set_b);
 
-			visited[city_b] = true;
 			if (city_a == AIRPORT) {
 				roads_airports.num_airports++;
 			} else {
-				visited[city_a];
 				roads_airports.num_roads++;
 			}
 			roads_airports.cost += cost;
+			visited[city_b] = true;
 
 		}
 	}
@@ -210,12 +209,9 @@ void Graph::min_span_tree(void) {
 	/* Taking road costs */
 	if (roads.num_roads == (int) _num_vertices-1) {
 		if (roads.cost <= roads_airports.cost) {
-			_final_roads = roads.num_roads;
-			_total_cost = roads.cost;
+			_budget = roads;
 		} else {
-			_final_roads = roads_airports.num_roads;
-			_final_airports = roads_airports.num_airports;
-			_total_cost = roads_airports.cost;
+			_budget = roads_airports;
 		}
 	} else {
 		for (Vertex city = 1; city <= _num_vertices; city++) {
@@ -223,11 +219,11 @@ void Graph::min_span_tree(void) {
 				_status = INSUFFICIENT;
 			}
 		}
-		_final_roads = roads_airports.num_roads;
-		_final_airports = roads_airports.num_airports;
-		_total_cost = roads_airports.cost;
+		_budget = roads_airports;
 	}
 
+	/* Cleaning up */
+	delete [] visited;
 }
 
 /***************************** MAIN function **********************************/
